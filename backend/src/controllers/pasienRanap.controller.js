85e4e5544435280e1394f440e8eee255c3862ffa
AndreasLampah <andreaslampah97@gmail.com>
2026-07-07 15:41:51 +0700

    update pasienRanap

--- backend/src/controllers/pasienRanap.controller.js
+++ backend/src/controllers/pasienRanap.controller.js
@@ -1,44 +1,10 @@
-import prisma from "../config/prisma.js";
+import { pasienRanapService } from "../services/pasienRanap.service.js";
 
 export const filterPasien = async (req, res) => {
   try {
     const { namaPasien } = req.body;
 
-    if (!namaPasien || !namaPasien.trim()) {
-      return res.status(400).json({
-        success: false,
-        message: "Nama Pasien harus di isi",
-      });
-    }
-
-    const nama = namaPasien.trim();
-
-    const pasien = await prisma.$queryRaw`
-  SELECT
-    rp.no_rawat AS nomor_rawat,
-    p.no_rkm_medis AS nomor_rekam_medis,
-    p.nm_pasien AS nama_pasien,
-    ki.kd_kamar,
-    ki.tgl_masuk,
-    ki.jam_masuk
-  FROM reg_periksa rp
-  INNER JOIN pasien p
-    ON rp.no_rkm_medis = p.no_rkm_medis
-  INNER JOIN kamar_inap ki
-    ON rp.no_rawat = ki.no_rawat
-  WHERE p.nm_pasien LIKE ${`%${nama}%`}
-    AND rp.status_lanjut = 'Ranap'
-    AND rp.stts != 'Batal'
-    AND ki.stts_pulang = '-'
-    LIMIT 50
-`;
-
-    if (pasien.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: `Pasien ${nama} tidak ditemukan`,
-      });
-    }
+    const pasien = await pasienRanapService(namaPasien);
 
     return res.status(200).json({
       success: true,
---
+++ backend/src/services/pasienRanap.service.js
@@ -0,0 +1,33 @@
+import prisma from "../config/prisma.js";
+
+export const pasienRanapService = async (namaPasien) => {
+  if (!namaPasien || !namaPasien.trim()) {
+    throw new Error("Nama pasien wajib di isi");
+  }
+
+  const nama = namaPasien.trim();
+
+  const pasienRanap = await prisma.$queryRaw`SELECT
+    rp.no_rawat AS nomor_rawat,
+    p.no_rkm_medis AS nomor_rekam_medis,
+    p.nm_pasien AS nama_pasien,
+    ki.kd_kamar,
+    ki.tgl_masuk,
+    ki.jam_masuk
+  FROM reg_periksa rp
+  INNER JOIN pasien p
+    ON rp.no_rkm_medis = p.no_rkm_medis
+  INNER JOIN kamar_inap ki
+    ON rp.no_rawat = ki.no_rawat
+  WHERE p.nm_pasien LIKE ${`%${nama}%`}
+    AND rp.status_lanjut = 'Ranap'
+    AND rp.stts != 'Batal'
+    AND ki.stts_pulang = '-'
+    LIMIT 50`;
+
+  if (pasienRanap.length === 0) {
+    throw new Error(`Pasien ${nama} tidak ditemukan`);
+  }
+
+  return pasienRanap;
+};
